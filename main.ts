2d55c803a5b4de489381337cc8a7d5646714ad2a
fabiang78 <105511071+fabiang78@users.noreply.github.com>
2022-08-18 18:54:12 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,49 @@
+let valor = 0
+basic.forever(function () {
+    valor = pins.analogReadPin(AnalogPin.P0)
+    if (valor < 200) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else if (valor < 300) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            # # . . .
+            `)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else if (valor >= 300 && valor < 400) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # . .
+            # # # . .
+            `)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else if (valor >= 400 && valor < 500) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # # .
+            . # # # .
+            # # # # .
+            `)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    } else {
+        basic.showLeds(`
+            . . . . #
+            . . . # #
+            . . # # #
+            . # # # #
+            # # # # #
+            `)
+    }
+})
